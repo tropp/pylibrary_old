30f7279403f55f2d4e487cfe67dae30a5f2e3681
Tessa-Jonne Ropp <tessa_jonne_ropp@med.unc.edu>
2021-05-10 15:28:43 -0400

    initial commit of old pylibrary

---
+++ pylibrary/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+__author__ = "Paul B. Manis"
+__version__ = "0.3"
+
+import pylibrary.Params
+import pylibrary.Fitting
+import pylibrary.PlotHelpers
+import pylibrary.RStats
+import pylibrary.Utility
+
+import pylibrary.bootstrap
+import pylibrary.matplotlibexporter
+import pylibrary.permutation
+import pylibrary.picker
+import pylibrary.pyqtgraphPlotHelpers
+import pylibrary.simplex
+import pylibrary.tau_lmfit
+import pylibrary.tifffile
+import pylibrary.titlecase
+
+# try:
+#     import RStats
+#     if listimports:
+#         print 'imported RStats'
+# except:
+#     raise ImportWarning("pylibrary: failed to import RStats")
